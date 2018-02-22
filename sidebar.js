8338ee6c3fb0c1a0db0b7135dc9b5171baf671ca
MaciejReimann <maciej.reimann@gmail.com>
2018-02-22 17:27:58 +0100

    sidebar module refactored using the constructor function

--- sidebar.js
+++ sidebar.js
@@ -1,14 +1,27 @@
 const SidebarComponent = function(content, attachedFunction) {
 	this.content = content;
 	this.attachedFunction = attachedFunction;
+
+	this.containerElement = document.createElement("DIV");
+	this.containerElement.className = "sidebarChildren";
+	this.containerElement.textContent = content;
+	
+	this.containerElement.addEventListener("click", this.attachedFunction.bind(this) )
 };
 
-SidebarComponent.prototype.render = function(parentElement, contentContainer) {
-	contentContainer.appendChild(this.content);
-	parentElement.appendChild(contentContainer);	
-};
+(function() {
+	const sidebarContainer = document.createElement("DIV");
+	sidebarContainer.className = "sidebarContainer";
+	const sidebarComponents = [
+			new SidebarComponent("Fill the table", (function() { console.log(this.containerElement.firstChild) }) ),
+			new SidebarComponent("Fill the gaps", (function() { console.log(this) }) ),
+			new SidebarComponent("Reveal the photo", (function() { console.log(this) }) ),
+			new SidebarComponent("Fast counting", (function() { console.log(this) }) ),
+	]
 
-SidebarComponent.prototype.fireAttachedFunction = function(function) {
-	attachedFunction
-};
+	sidebarComponents.forEach(function(item) {
+		sidebarContainer.appendChild( item.containerElement )
+	});
+	document.querySelector(".sidebar").appendChild (sidebarContainer);
+})();
 
