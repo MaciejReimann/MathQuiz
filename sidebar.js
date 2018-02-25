d71a5fe593b02ecf1debbc6c2bf2c11c9196695a
MaciejReimann <maciej.reimann@gmail.com>
2018-02-25 23:51:37 +0100

    keypressController added

--- sidebar.js
+++ sidebar.js
@@ -24,6 +24,7 @@ Equation.prototype.setBlankPosition = function(p) {
 };
 
 const EquationsArray = function(givenArray) {
+	this.index = 0;
 	this.array = new Array();
 	let loopCounter = -1;
 	for (let i = 1; i < 10; i++ ) {       
@@ -40,18 +41,35 @@ EquationsArray.prototype.setBlankPositionForAll = function(p) {
 		equation.blankPosition = p;
 	})
 };
+EquationsArray.prototype.getCurrentElement = function() { return this.array[this.index] };
+const StoredValue = function(value) {
+  this.value = value;
+};
+StoredValue.prototype.set = function(n) {this.value = n};
+StoredValue.prototype.get = function(n) {return this.value};
+
+const Score = function(initialValue) {
+	if (initialValue) {
+		this.globalScore = initialValue;
+	} else { this.globalScore = 0 };  
+  this.strike = 0;
+  // this.getBaseFrom = function(n) { localBase = n }; // How to define parameteras an anonymous function?""
+};
+Score.prototype.strikeIncrement = function() { this.strike ++ };
+Score.prototype.strikeReset = function() { this.strike = 0 };
+Score.prototype.increment = function() { this.globalScore = this.globalScore + this.strike * 2 };
+Score.prototype.get = function() { return this.globalScore };
 
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
 
 const model = {
 	array: new EquationsArray(),
+	activeFunctionIndex: new StoredValue(0),
 	currentIndexes:[],
 	// activeFunctionIndex: new StoredValue(0),
-	currentState: new Array(),
-	update: function() {
-
-	},
+	// currentState: new Array(),
+	score: new Score(),
 };
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -61,38 +79,24 @@ const model = {
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
 
-const StoredValue = function(value) {
-  this.value = value;
-};
-StoredValue.prototype.set = function(n) {this.value = n};
-StoredValue.prototype.get = function(n) {return this.value};
-
-////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////
-
 const controller = function() {
-	// currentState = new Array2D( new Array() )
-	this.array = [
+	const array = [
 		(function() {
 			model.array.setBlankPositionForAll("z");
 			return model.array.getOrdered();
 		}),		
 	];
-	const currentIndexes = model.currentIndexes;
+	let getActiveFunctionIndex = model.activeFunctionIndex.get();
 	return {
-		array: array,
-		currentIndex: function() {
-			return {
-				set: function(i, n) {currentIndexes[i] = n },
-				get: function(i) { return currentIndexes[i] }
-			}
-		},
-		
-		// setActiveFunctionIndex: function(n) { model.activeFunctionIndex.set(n) },
-		// getActiveFunctionIndex: function() { return model.activeFunctionIndex.get() },
-		setCurrentState: function() { },
-		getCurrentState: function() { },
-	}
+		setActiveFunctionIndex: function(n) { activeFunctionIndex.set(n) },
+		getActiveFunctionIndex: function() { return getActiveFunctionIndex },
+
+		getCurrentArrayState: function() { return array[getActiveFunctionIndex] () },
+		getCurrentElement: function() { return this.getCurrentArrayState()[ this.getCurrentArrayIndex() ] },
+
+		setCurrentArrayIndex: function(n) {model.currentIndexes[ getActiveFunctionIndex ] = n},
+		getCurrentArrayIndex: function() { return model.currentIndexes[ getActiveFunctionIndex ] },
+	};
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -115,6 +119,7 @@ const createTable = function(array) {
       containerElement.appendChild(square);
       const inputField = createEl("input", "input-fields" );
       inputField.id = equation.index - 10;
+      inputField.setAttribute("index", equation.index);
       square.appendChild(inputField);
       inputField.value = equation.userAnswer; // if there is one entered previously;
       if (inputField.value !== "") { // marked as "done"; otherwise it would be editable
@@ -133,49 +138,130 @@ const createEl = function(tag, className, textContent) {
 
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
-
-const view = {
+const view = {}
+view.score = {
+	parentElement: document.querySelector('.score'),
+  render: function() {
+    let content = model.score.get();
+    this.clear();
+    this.parentElement.appendChild( createEl("DIV", "scoreDiv", content) );
+    },
+   clear: function() {
+    if (this.parentElement.firstChild) {
+        this.parentElement.removeChild(this.parentElement.firstChild);
+      };
+    }, 
+}
+view.main = {
 	parentElement: document.querySelector(".main"),
-	activeFunctionIndex: new StoredValue(0).get() ,
-	currentArray: controller().array[0](),
+	activeFunctionIndex: controller().getActiveFunctionIndex(),
+	currentArrayState: controller().getCurrentArrayState(),
+
 	pages: [
 			(function(n) {return createTable(n)}),
 	],
+	events: [
+
+	],
 	render: function() {
 		this.clear();
-		this.parentElement.appendChild(this.pages[this.activeFunctionIndex](this.currentArray));
-		eventHandlers().inputFields.focusOnFirst();			
+		// view.score.render()
+		this.parentElement.appendChild(this.pages[this.activeFunctionIndex](this.currentArrayState));
+		// eventHandlers().inputFields.focusOnFirst();			
 	},
 	clear: function() {
 		if (this.parentElement.firstChild) {
 			this.parentElement.removeChild(this.parentElement.firstChild);
 		}
 	},
-}
+};
+
+
+const updateView = function() {
+	for (component in view) { view[component].render() }
+	
+};
+
+
+// console.log( keypressController.events.Enter())
 
 const events = function() {
 	const inputFields = Array.from(document.querySelectorAll("input"));
-
 	const tableEvents = function() {
 		inputFields.map(function(item) {
 			eventHandlers().inputFields.focusOnFirst();
-			// currentEquation = item.id !!!!!!!!!!!!!!!!!!!!!
-			item.addEventListener("input", function() { eventHandlers().inputFields.validate(item.value) })
+			item.addEventListener("input", function() { console.log(this.value) })
+			item.addEventListener("keydown", function() { keypressController.fireHandler(event.code) })
+			// item.addEventListener("change", function() { updateView() })
 		})
 	}
 	return {
-		table: function() {
-				tableEvents()
-		},
+		tableEvents: tableEvents,
 	}
 };
+
+keypressController = {
+	events: {
+		Enter: (function() {console.log("enter") } ),
+		ArrowDown: (function() {console.log("down") } ),
+		ArrowUp: (function() {console.log("up") } ),
+		ArrowLeft: (function() {console.log("left") } ),
+		ArrowRight: (function() {console.log("right") } ),
+	},
+	fireHandler: function(name) {
+		for (key in this.events) {
+			if (key === name) { this.events[key]() }
+		}
+	},		
+}
+
+inputController = {
+	validate: 
+}
+
+
+
+const checkInput = function() {
+	const setCurrentEquation = function() {
+		let activeElementIndex = parseInt(document.activeElement.getAttribute("index"))
+		controller().setCurrentArrayIndex(activeElementIndex);
+	}();
+	const getCurrentEquation =  function() {return controller().getCurrentElement() };
+	return {
+		validate: function(inputValue) {
+			if ( isNaN( inputValue ) ) {
+        proceed().ifInvalid( element );
+      } else {
+        proceed().ifValid( element );
+      };
+		},
+		ifCorrect: function(inputValue) {
+			if ( getCurrentEquation().checkAnswer(parseInt(inputValue)) ) {
+				model.score.strikeIncrement();
+        model.score.increment();
+			} else {
+				model.score.strikeReset();
+			};
+			// console.log(getCurrentEquation().checkAnswer(inputValue))
+		},
+	}
+}
+
+
 
 const eventHandlers = function() {
 	const inputFields = document.querySelectorAll("input");
 	const activeID = function(){ return parseInt(document.activeElement.id) } 
 	return {
+		onEnter: function(event) {
+			if(event.keyCode = 13) {console.log("enter")}
+		},
+		onChange: function(inputValue) {
+				checkInput().ifCorrect(inputValue);
+				updateView();
+		},
 		inputFields: {
-			validate: function(inputValue) { console.log ( inputValue )},
+
 			focusOnFirst: function() {
 				inputFields[0].focus()
 				// console.log(activeID() )
@@ -198,42 +284,13 @@ const eventHandlers = function() {
 }
 
 
-view.render()
-events().table()
-
-
-
-keyEvents : {
-
-
-
-}
-
-
-
-
-  // controller.table.array.getInOrder().map(function( equation ) {
-  //             if (equation.x === 1 || equation.y === 1 ) { 
-  //               let square = createEl("div", "first-rows", equation.z );
-  //               parentEl.appendChild(square);
-  //             } else {
-  //               let square = createEl("div", "the-rest");
-  //               parentEl.appendChild(square);
-  //               const inputfields = createEl("input", "input-fields" );
-  //               square.appendChild(inputfields);
-  //               inputfields.value = equation.userAnswer; // if there is one entered previously;
-  //               if (inputfields.value !== "") { // marked as "done"; otherwise it would be editable
-  //                 controller.equations.proceed().ifDone(inputfields)
-  //               };                
-  //             };
-  //           });
-  //         controller.table.array.setInputPositionForAll("z");
-  //         view.table.events();
+updateView()
+// view.render()
+events().tableEvents()
 
 const SidebarComponent = function(content, attachedFunction) {
 	this.content = content;
 	this.attachedFunction = attachedFunction;
-
 	this.containerElement = document.createElement("DIV");
 	this.containerElement.className = "sidebarChildren";
 	this.containerElement.textContent = content;
