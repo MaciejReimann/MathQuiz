216a37815b667286dc54997bbf452729e61a056e
MaciejReimann <maciej.reimann@gmail.com>
2018-03-20 23:37:49 +0100

    ClickableComponent() added

--- mtView.js
+++ mtView.js
@@ -10,34 +10,39 @@ function viewModule() {
 	const mainIndex = function() {return controller().getMainIndex()} 
 	const mainContent = function() {return controller().getMainContent(mainIndex())}  
 	// let sidebarComponents = []
-	const SidebarComponent = function(content, attachedFunction) {
+	const ClickableComponent = function(content, attachedFunction) {
 		this.divElement = createElement( "DIV", "sidebarChildren", content);	
 		this.divElement.addEventListener("click", attachedFunction.bind(this) )
 	};
-	SidebarComponent.prototype.setActiveAttribute = function() {
+	ClickableComponent.prototype.activeAttributeOn = function() {
 		this.divElement.setAttribute("active", true);
+	};
+		ClickableComponent.prototype.activeAttributeOff = function() {
+		this.divElement.setAttribute("active", false);
 	};
 	function createSidebar() {
 		const containerElement = createElement( "DIV", "sidebarContainer");
 		const sidebarComponents = [
-				new SidebarComponent("Fill the table", (function() { view.update(0) }) ),
-				new SidebarComponent("Fill the gaps", (function() { view.update(1) }) ),
-				new SidebarComponent("Reveal the photo", (function() { view.update(2) }) ),
-				new SidebarComponent("Fast counting", (function() { view.update(3) }) ),
+				new ClickableComponent("Fill the table", (function() { view.update(0) }) ),
+				new ClickableComponent("Fill the gaps", (function() { view.update(1) }) ),
+				new ClickableComponent("Reveal the photo", (function() { view.update(2) }) ),
+				new ClickableComponent("Fast counting", (function() { view.update(3) }) ),
 		];
 		sidebarComponents.forEach(function(item) {
 			containerElement.appendChild( item.divElement );
 		})
 		const setActiveAttribite = function() {
-			sidebarComponents[mainIndex()].setActiveAttribute(); // calling controller HERE!
+			if (mainIndex() < 4) { sidebarComponents[mainIndex()].activeAttributeOn() } else { // calling controller HERE!
+				sidebarComponents.forEach(function(item) {item.activeAttributeOff() });
+			}
 		}();
 		return containerElement;
 	};
 	function createResults() {
-		const content = function() {
-			return "RESULTS"
-		}
-		return createElement( "DIV", "resultsDiv", resultsContent()); // calling controller HERE!
+		const containerElement = createElement( "DIV", "resultsContainer");
+		let content = new ClickableComponent( "Results", (function() { view.update(4) }) );
+		containerElement.appendChild(content.divElement)
+		return containerElement; 
 	}
 	function createScore() {
 		return createElement( "DIV", "scoreDiv", scoreContent()); // calling controller HERE!
