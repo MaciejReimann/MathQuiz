ec87fbcc0c506642f5bd5193d6b6913a7a976667
MaciejReimann <maciej.reimann@gmail.com>
2018-04-01 22:55:39 +0200

    update after model.page refactored

--- mtController.js
+++ mtController.js
@@ -19,6 +19,8 @@ const controller = function() {
 	const score = model.score;
 	const arr = model.array;
 	const array = model.array.get();
+	const page = model.page;
+
 	const pages = model.pages
 	const activePageIndex = model.activePageIndex;
 	const index = function() {
@@ -26,13 +28,16 @@ const controller = function() {
 			return activePageIndex.get();		
 		}
 	};
-	const getActiveData = function() {return pages[index()] }
-	const getSequence = function() {return getActiveData()[0]}
-	const getSequenceIndex = function() {return getActiveData() [1] }	
-	const getArrayIndex = function() {return getSequence()[getSequenceIndex()]}
-	const getActiveEquation = function() {return array[ getArrayIndex() ] };
-	const setArrayIndex = function(n) {pages[index()][1] = n};
-	const incrementArrayIndex = function() { pages[index()][1]++ };
+	const setSequenceForIncorrect = function() {
+		arr.getAllEquationsAnsweredIncorrectly().map(function(equation) {
+			page[1].sequence.push(equation.index)
+		})
+	}();
+	const getActivePage = function() {return page[index()]}
+	const getArrayIndex = function() {return getActivePage().sequence[getActivePage().index]}
+	const getActiveEquation = function() {return array[getArrayIndex()]}
+	const setSequenceIndex = function(n) {getActivePage().index = n}; 
+	const incrementSequenceIndex = function() { return getActivePage().index++};
 
 	const getScore = function() {return score.get()}
 	const getMainContent = function(n) {
@@ -43,42 +48,20 @@ const controller = function() {
 		const firstActive = document.getElementsByClassName("active")[0]
 		if (firstActive!==undefined) {return firstActive.value} else { return false }
 	};		
+
 	const lastEquationAnswered = function() {
 		if( getActiveElement() !== "" ) {return true} else { return false}
 	};
 	function informed() {
 		function whenViewUpdated(v) {
-			if (lastEquationAnswered()) { incrementArrayIndex() };
+			logActiveEquation('informed')
+			if (lastEquationAnswered()) { incrementSequenceIndex() };
 			activePageIndex.set(v);			
 		}
 		return {
 			 whenViewUpdated:  whenViewUpdated,
 		}
-	}
-
-	function insertResults() {
-		const containerElement = createElement( "DIV", "table");
-		array.map(function(equation) {
-			let tableSquare = equation.elementZ;
-			let sumOfCorrectAnswers = arr.getSumOfAnswersForMirrorElements(equation.x, equation.y).correct
-			let sumOfIncorrectAnswers = arr.getSumOfAnswersForMirrorElements(equation.x, equation.y).incorrect
-			if (equation.x > equation.y) { tableSquare.setAs("invisible") } else {
-				let sum = sumOfCorrectAnswers;
-				if (sum > 2) {
-					tableSquare.setAs("excellent");
-	 			} else if (sum > 1) {
-	 				tableSquare.setAs("very-good");
-	 			} else if (sum > 0) {
-	 				tableSquare.setAs("good");
-	 			} else { 
-	 				tableSquare.setAs("not-answered");
-	 			}
-			}	
-			containerElement.appendChild(tableSquare.getElement());	
-		})
-		return containerElement;
 	};
-
 	function insertTable () {
 		const containerElement = createElement( "DIV", "table");
 		let id = 0;
@@ -99,12 +82,13 @@ const controller = function() {
 		})
 		return containerElement;
 	};
-	function insertEquationParagraph (activeEquation) {
-		const containerElement = createElement( "DIV", "equationParagraph")
+	function insertEquationParagraph () {
+		logActiveEquation('insertEquationParagraph')
+		const containerElement = createElement( "DIV", "equationParagraph");
 		getActiveEquation().createLeftSide(4).map(function(item) {
 			containerElement.appendChild(item)
 		})
-	return containerElement;
+		return containerElement;
 	};
 
 	function insertArea (activeEquation) {
@@ -149,7 +133,6 @@ const controller = function() {
 	  	containerElement.appendChild(tableSquareContainer);
 	  	return containerElement;
 	}
-
 const insertPhoto = function(activeEquation) {
 	const containerElement = createElement( "DIV", "photo");
 
@@ -180,13 +163,44 @@ const insertPhoto = function(activeEquation) {
 //                 canvasWidth / 10, canvasHeight / 10 // ?
 //   );
 // });
+
   containerElement.appendChild(canvas);
-  containerElement.appendChild(insertEquationParagraph(getActiveEquation()));
+  containerElement.appendChild(insertEquationParagraph());
 
   return containerElement;
 };
-
+function insertResults() {
+	const containerElement = createElement( "DIV", "table");
+	array.map(function(equation) {
+		let tableSquare = equation.elementZ;
+		let sumOfCorrectAnswers = arr.getSumOfAnswersForMirrorElements(equation.x, equation.y).correct
+		let sumOfIncorrectAnswers = arr.getSumOfAnswersForMirrorElements(equation.x, equation.y).incorrect
+		if (equation.x > equation.y) { tableSquare.setAs("invisible") } else {
+			let sum = sumOfCorrectAnswers;
+			if (sum > 2) {
+				tableSquare.setAs("excellent");
+ 			} else if (sum > 1) {
+ 				tableSquare.setAs("very-good");
+ 			} else if (sum > 0) {
+ 				tableSquare.setAs("good");
+ 			} else { 
+ 				tableSquare.setAs("not-answered");
+ 			}
+		}	
+		containerElement.appendChild(tableSquare.getElement());	
+	})
+	return containerElement;
+};
+function logActiveEquation (text) {
+	console.log( text )
+	console.log("active equation is: ", getActiveEquation() )
+	// console.log( "SEQUENCE INDEX IS: ", getSequenceIndex() )
+	console.log( "ACTIVE FUNCTION IS: ", getActivePage() )
+	
+	
+}
 function proceedWhen(input) {
+	// logActiveEquation()
 	let inputElement = input.element;
 	function isInvalid() { inputElement.setAttribute("valid", false) };
 	function isValid() { inputElement.setAttribute("valid", true) };
@@ -196,6 +210,7 @@ function proceedWhen(input) {
 		viewModule().update( mainIndex() )
 	};
 	function isCorrect() {
+		
 		getActiveEquation().answeredCorrectly.push(true);
 		score.updateWhenCorrect();
 		viewModule().update( mainIndex() )
@@ -216,7 +231,11 @@ function proceedWhen(input) {
 		informed: informed,
 		proceedWhen: proceedWhen,
 		
-		setArrayIndex: setArrayIndex,
+		setSequenceIndex: setSequenceIndex,
 		getArrayIndex: getArrayIndex,
+
+		logActiveEquation: logActiveEquation,
+		getActivePage: getActivePage,
+
 	};
 }
