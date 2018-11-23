871c2a6368b93ada58769daa47f95341788ce48d
MaciejReimann <maciej.reimann@gmail.com>
2018-11-23 21:18:50 +0100

    first draft of app logic

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,3 +6,69 @@ helper();
 
 console.log("Hello from index.js!")
 
+
+
+/*
+HELPERS
+
+function generateEquations(settings) {
+} // 
+// 
+
+const currentEquation = generateEquations(initialState.equationsShape)[counter].elements // ["1", "*", "1", "=", "_"]
+const currentEquation = generateEquations(initialState.equationsShape)[counter].solution // 1
+
+0. Initial state
+
+const equationsShape = {
+    operation: "*",
+    singleTerm: "right",
+    blank: "last",
+    rangeStart: 0,
+    rangeEnd: 100,
+    length: 10
+}
+
+const currentEquation = generateEquation(equationShape);
+
+const user = null;
+
+const score = 0;
+
+const counter = 0;
+
+const initialState = {
+    equationsShape, 
+    currentEquation,
+    user,
+    score
+};
+
+
+
+1.Walidation & evaluation of user's input:
+
+const answer = document.querySelector("blank").value
+
+
+function isAnswerValid(input) {
+}
+
+
+
+function isAnswerCorrect(input) {
+    currentEquation.solution === input ? 
+}
+
+const solvedEquation = isAnswerCorrect(answer) ? currentEquation.concat({answered: })
+
+
+
+
+isAnswerCorrect(inputValue)
+
+state.equationsSolved.concat(currentEquation)
+
+
+*/
+
